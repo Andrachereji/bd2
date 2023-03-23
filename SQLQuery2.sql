80e7161e4063adadf5a90fc08dafd21430d56329
Andra Chereji <91540637+Andrachereji@users.noreply.github.com>
2023-03-23 14:59:39 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,7 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT * from Animal
+Select * from Medic
+Select * from Consultatie
+Select * from Medicament
+Select * from EvidentaMedicamente
+Select * from Asistenta
